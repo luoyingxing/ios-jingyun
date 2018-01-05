c8f14f9286debed81ac497a8ba883975912042cd
luoyingxing <https://github.com/luoyingxing/FirstOCProject.git>
2018-01-05 15:55:27 +0800

    add device detail.

---
+++ ios-jingyun/App/Device/Controllers/DeviceDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeviceDetailViewController.h
+//  ios-jingyun
+//
+//  Created by conwin on 2018/1/5.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeviceDetailViewController : UIViewController
+
+@end
