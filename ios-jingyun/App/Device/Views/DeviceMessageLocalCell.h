0158e71488919c16795b534b980a09a500b9136a
luoyingxing <https://github.com/luoyingxing/FirstOCProject.git>
2018-01-05 16:51:04 +0800

    add DeviceMessageLocalCell

---
+++ ios-jingyun/App/Device/Views/DeviceMessageLocalCell.h
@@ -0,0 +1,13 @@
+//
+//  DeviceMessageLocalCell.h
+//  ios-jingyun
+//
+//  Created by conwin on 2018/1/5.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeviceMessageLocalCell : UITableViewCell
+
+@end
---
+++ ios-jingyun/App/Device/Views/DeviceMessageLocalCell.m
@@ -0,0 +1,24 @@
+//
+//  DeviceMessageLocalCell.m
+//  ios-jingyun
+//
+//  Created by conwin on 2018/1/5.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import "DeviceMessageLocalCell.h"
+
+@implementation DeviceMessageLocalCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
