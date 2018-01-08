cd128b2704be02430151c086c0e6fefdfb09d924
luoyingxing <https://github.com/luoyingxing/FirstOCProject.git>
2018-01-08 18:13:14 +0800

    add collective  cell

---
+++ ios-jingyun/App/Device/Views/ZoneViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ZoneViewCell.h
+//  ios-jingyun
+//
+//  Created by conwin on 2018/1/8.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZoneViewCell : UICollectionViewCell
+
+@end
---
+++ ios-jingyun/App/Device/Views/ZoneViewCell.m
@@ -0,0 +1,18 @@
+//
+//  ZoneViewCell.m
+//  ios-jingyun
+//
+//  Created by conwin on 2018/1/8.
+//  Copyright © 2018年 conwin. All rights reserved.
+//
+
+#import "ZoneViewCell.h"
+
+@implementation ZoneViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+@end
